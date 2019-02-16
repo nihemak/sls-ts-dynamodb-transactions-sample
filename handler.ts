33f971beef0ad318335426149cad91acb47ce519
Makoto Nihei <nihemak@gmail.com>
2019-02-16 21:23:26 +0900

    Add insert of dynamodb table using transaction

--- handler.ts
+++ handler.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import * as AWS from 'aws-sdk';
+import * as uuid from 'uuid';
 const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10', region: process.env.REGION});
 
 const category_id = '9E66EBBF-A3AE-4490-8AC6-CB9AEC15C1F7';
@@ -36,15 +37,33 @@ export const init: APIGatewayProxyHandler = async (event, context) => {
   };
 }
 
-function putCategory(limit: number): Promise<void> {
+export const add: APIGatewayProxyHandler = async (event, context) => {
+  let statusCode = 200;
+  let message = 'ok';
+  try {
+    await addItem();
+  } catch (err) {
+    statusCode = 500;
+    message = `error: ${err}`;
+  }
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify({
+      message: message,
+      input: event,
+    }),
+  };
+}
+
+function putCategory(restCount: number): Promise<void> {
   const params = {
     TableName: `${process.env.DYNAMO_PREFIX}-transaction-categories`,
     Item: {
       id: {
         S: category_id
       },
-      limit: {
-        N: `${limit}`
+      restCount: {
+        N: `${restCount}`
       }
     }
   };
@@ -71,6 +90,53 @@ function scanItems(): Promise<any> {
         return reject(err);
       } else {
         return resolve(data.Items)
+      }
+    });
+  });
+}
+
+function addItem(): Promise<void> {
+  const params = {
+    TransactItems: [
+      {
+        Update: {
+          TableName: `${process.env.DYNAMO_PREFIX}-transaction-categories`,
+          Key: {
+            id: {
+              S: category_id
+            }
+          },
+          ConditionExpression: 'restCount > :none',
+          UpdateExpression: 'SET restCount = restCount - :one',
+          ExpressionAttributeValues: {
+            ':none': {
+              N: '0'
+            },
+            ':one': {
+              N: '1'
+            }
+          }
+        }
+      },
+      {
+        Put: {
+          TableName: `${process.env.DYNAMO_PREFIX}-transaction-items`,
+          Item: {
+            id: {
+              S: uuid.v4()
+            }
+          }
+        }
+      }
+    ]
+  };
+  return new Promise((resolve, reject) => {
+    dynamodb.transactWriteItems(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+        return reject(err);
+      } else {
+        return resolve()
       }
     });
   });
